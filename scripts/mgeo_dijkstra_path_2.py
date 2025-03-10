65b203cc19a9f847bfd0e3236162c33d1a6ea820
jhlim-morai <120444462+jhlim-morai@users.noreply.github.com>
2025-03-10 13:27:20 +0900

    english

--- scripts/mgeo_dijkstra_path_2.py
+++ scripts/mgeo_dijkstra_path_2.py
@@ -19,23 +19,6 @@ sys.path.append(current_path)
 from lib.mgeo.class_defs import *
 
 
-# mgeo_dijkstra_path_1 은 Mgeo 데이터를 이용하여 시작 Node 와 목적지 Node 를 지정하여 Dijkstra 알고리즘을 적용하는 예제 입니다.
-# 사용자가 직접 지정한 시작 Node 와 목적지 Node 사이 최단 경로 계산하여 global Path(전역경로) 를 생성 합니다.
-# 시작 Node 와 목적지 Node 는 Rviz 의 goal pose / initial pose 두 기능을 이용하여 정의합니다.
-
-# 노드 실행 순서 
-# 1. Mgeo data 읽어온 후 데이터 확인
-# 2. 시작 Node 와 종료 Node 정의
-# 3. weight 값 계산
-# 4. Dijkstra Path 초기화 로직
-# 5. Dijkstra 핵심 코드 
-# 6. node path 생성
-# 7. link path 생성
-# 8. Result 판별
-# 9. point path 생성
-# 10. dijkstra 경로 데이터를 ROS Path 메세지 형식에 맞춰 정의
-# 11. dijkstra 이용해 만든 Global Path 정보 Publish 
-
 class dijkstra_path_pub :
     def __init__(self):
         rospy.init_node('dijkstra_path_pub', anonymous=True)
@@ -45,7 +28,6 @@ class dijkstra_path_pub :
         rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.goal_callback)
         rospy.Subscriber('/initialpose', PoseWithCovarianceStamped, self.init_callback)
 
-        #TODO: (1) Mgeo data 읽어온 후 데이터 확인
         load_path = os.path.normpath(os.path.join(current_path, 'lib/mgeo_data/kcity'))
         mgeo_planner_map = MGeoPlannerMap.create_instance_from_json(load_path)
 
@@ -80,7 +62,6 @@ class dijkstra_path_pub :
             # make global path
             self.global_path_msg = self.calc_dijkstra_path_node(self.start_node, self.end_node)
 
-            #TODO: (11) dijkstra 이용해 만든 Global Path 정보 Publish
             self.global_path_pub.publish(self.global_path_msg)
             rate.sleep()
     
@@ -88,8 +69,6 @@ class dijkstra_path_pub :
         if self.is_init_pose is not False:
             self.is_goal_pose == False
 
-        #TODO: (2) 시작 Node 와 종료 Node 정의
-        # 시작 Node 는 Rviz 기능을 이용해 지정한 위치에서 가장 가까이 있는 Node 로 한다.
         start_min_dis=float('inf')
         self.init_msg=msg
         self.init_x=self.init_msg.pose.pose.position.x
@@ -106,8 +85,7 @@ class dijkstra_path_pub :
         self.is_init_pose = True
 
     def goal_callback(self,msg):
-        #TODO: (2) 시작 Node 와 종료 Node 정의
-        # 종료 Node 는 Rviz 기능을 이용해 지정한 위치에서 가장 가까이 있는 Node 로 한다.
+
         goal_min_dis=float('inf')
         self.goal_msg = msg
         self.goal_x=self.goal_msg.pose.position.x
@@ -128,7 +106,6 @@ class dijkstra_path_pub :
 
         result, path = self.global_planner.find_shortest_path(start_node, end_node)
 
-        #TODO: (10) dijkstra 경로 데이터를 ROS Path 메세지 형식에 맞춰 정의
         out_path = Path()
         out_path.header.frame_id = '/map'
 
@@ -151,32 +128,24 @@ class Dijkstra:
         self.lane_change_link_idx = []
 
     def get_weight_matrix(self):
-        #TODO: (3) weight 값 계산
-        # 초기 설정
+
         weight = dict() 
         for from_node_id, from_node in self.nodes.items():
-            # 현재 노드에서 다른 노드로 진행하는 모든 weight
             weight_from_this_node = dict()
             for to_node_id, to_node in self.nodes.items():
                 weight_from_this_node[to_node_id] = float('inf')
-            # 전체 weight matrix에 추가
             weight[from_node_id] = weight_from_this_node
 
         for from_node_id, from_node in self.nodes.items():
-            # 현재 노드에서 현재 노드로는 cost = 0
             weight[from_node_id][from_node_id] = 0
 
             for to_node in from_node.get_to_nodes():
-                # 현재 노드에서 to_node로 연결되어 있는 링크를 찾고, 그 중에서 가장 빠른 링크를 찾아준다
                 shortest_link, min_cost = self.find_shortest_link_leading_to_node(from_node,to_node)
                 weight[from_node_id][to_node.idx] = min_cost           
 
         return weight
     
     def find_shortest_link_leading_to_node(self,from_node, to_node):
-        """현재 노드에서 to_node로 연결되어 있는 링크를 찾고, 그 중에서 가장 빠른 링크를 찾아준다"""
-        #TODO: (3) weight 값 계산
-        # NOTE: 
         to_links = []
         for link in from_node.get_to_links():
             if link.to_node is to_node:
@@ -208,9 +177,7 @@ class Dijkstra:
 
     def find_shortest_path(self, start_node_idx, end_node_idx): 
         print(start_node_idx, end_node_idx)
-        #TODO: (4) Dijkstra Path 초기화 로직
-        # s 초기화         >> s = [False] * len(self.nodes)
-        # from_node 초기화 >> from_node = [start_node_idx] * len(self.nodes)
+
         s = dict()
         from_node = dict() 
         for node_id in self.nodes.keys():
@@ -220,7 +187,6 @@ class Dijkstra:
         s[start_node_idx] = True
         distance =copy.deepcopy(self.weight[start_node_idx])
 
-        #TODO: (5) Dijkstra 핵심 코드
         for i in range(len(self.nodes.keys()) - 1):
             selected_node_idx = self.find_nearest_node_idx(distance, s)
             s[selected_node_idx] = True            
@@ -231,7 +197,6 @@ class Dijkstra:
                         distance[to_node_idx] = distance_candidate
                         from_node[to_node_idx] = selected_node_idx
 
-        #TODO: (6) node path 생성
         tracking_idx = end_node_idx
         node_path = [end_node_idx]
         
@@ -241,7 +206,6 @@ class Dijkstra:
 
         node_path.reverse()
 
-        #TODO: (7) link path 생성
         link_path = []
         for i in range(len(node_path) - 1):
             from_node_idx = node_path[i]
@@ -253,11 +217,9 @@ class Dijkstra:
             shortest_link, min_cost = self.find_shortest_link_leading_to_node(from_node,to_node)
             link_path.append(shortest_link.idx)
 
-        #TODO: (8) Result 판별
         if len(link_path) == 0:
             return False, {'node_path': node_path, 'link_path':link_path, 'point_path':[]}
 
-        #TODO: (9) point path 생성
         point_path = []        
         for link_id in link_path:
             link = self.links[link_id]
