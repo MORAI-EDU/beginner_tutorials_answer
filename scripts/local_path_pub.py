e1e7d07634156f310067610361b02fb2bd06be0a
jhlim-morai <120444462+jhlim-morai@users.noreply.github.com>
2025-03-10 13:04:52 +0900

    english

--- scripts/local_path_pub.py
+++ scripts/local_path_pub.py
@@ -18,7 +18,7 @@ class path_pub :
 
         self.local_path_pub = rospy.Publisher('/local_path',Path, queue_size=1)
         
-        # 초기화
+        # initialize
         self.global_path_msg=Path()
         self.global_path_msg.header.frame_id='/map'
         
@@ -85,4 +85,3 @@ if __name__ == '__main__':
         test_track=path_pub()
     except rospy.ROSInterruptException:
         pass
-
